1cd389e12dde252e298ca03c7f9baaad7dd699be
Ryan Karl <rkarl@nd.edu>
2018-04-27 17:40:07 -0400

    feel the power of thor

--- thor.py
+++ thor.py
@@ -27,12 +27,65 @@ def usage(status=0):
 
 def do_request(pid):
     ''' Perform REQUESTS HTTP requests and return the average elapsed time. '''
-    return 0
+    elapsedTime = 0;
+    for request in range(REQUESTS): 
+        start = time.time()
+        r = requests.get(URL)
+        end = time.time()
+        if r.status_code != 200:
+            print("Error. Problem with request")
+        if VERBOSE:
+            print(r.text)
+        print("Process: {}, Request: {}, Elapsed Time: {:.2f}".format(pid,request,end-start))
+        elapsedTime += end-start
+
+    print("Process: {}, AVERAGE   , Elapsed Time: {:.2f}".format(pid,elapsedTime/REQUESTS))
+   
+
+    return elapsedTime/REQUESTS
 
 # Main execution
 
 if __name__ == '__main__':
     # Parse command line arguments
+    args = sys.argv[1:]
+    while args and args[0].startswith('-') and len(args[0]) > 1:
+        arg = args.pop(0)
+        if arg == "-v":
+            VERBOSE = True
+        elif arg == "-p":
+            PROCESSES = int(args.pop(0))
+        elif arg == "-r":
+            REQUESTS = int(args.pop(0))
+        elif arg == "-h":
+            usage(0)
+
+    if len(args) == 1:
+        URL = args.pop()
+    else:
+        usage(1)
+
+
+
+    print(PROCESSES, REQUESTS, VERBOSE, URL) 
+
+    
+
+        ##error("client: socket");
+
+
+        # Allow for multiprocessing based on number of cores specified
+    CORES = multiprocessing.cpu_count()
+    if PROCESSES < CORES:
+        CORES = PROCESSES 
+    pool = multiprocessing.Pool(CORES)
+    timeList = pool.map(do_request,range(PROCESSES))
+    totalTime = 0
+    for num in timeList:
+       totalTime += num 
+    print("TOTAL AVERAGE ELAPSED TIME: {:.2f}".format(totalTime/PROCESSES))
+
+
 
     # Create pool of workers and perform requests
     pass
